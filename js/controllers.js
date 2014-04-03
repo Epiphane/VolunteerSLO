4b3af97cdb3ce45aeb23786dda88646d55df8f29
Thomas Steinke <exyphnos@gmail.com>
2014-04-02 17:11:02 -0700

    Got some repeating up in here

--- js/controllers.js
+++ js/controllers.js
@@ -12,6 +12,26 @@ var volunteerSLOControllers = angular.module('volunteerSLOControllers', []);
 volunteerSLOControllers.controller('HomepageCtrl', ['$scope',
    function($scope) {
       $scope.name = 'Volunteer SLO';
+      $scope.banner = 'What are you waiting for?';
+
+      $scope.events = [
+         { id: 0, title: 'Paint Bob\'s fence!', description: 'Let\'s all go paint a fence!',
+            volunteers: 9, signedUp: 6 },
+         { id: 0, title: 'Paint Bob\'s fence!', description: 'Let\'s all go paint a fence!',
+            volunteers: 9, signedUp: 6 },
+         { id: 0, title: 'Paint Bob\'s fence!', description: 'Let\'s all go paint a fence!',
+            volunteers: 9, signedUp: 6 },
+         { id: 0, title: 'Paint Bob\'s fence!', description: 'Let\'s all go paint a fence!',
+            volunteers: 9, signedUp: 6 },
+         { id: 0, title: 'Paint Bob\'s fence!', description: 'Let\'s all go paint a fence!',
+            volunteers: 9, signedUp: 6 },
+         { id: 0, title: 'Paint Bob\'s fence!', description: 'Let\'s all go paint a fence!',
+            volunteers: 9, signedUp: 6 }
+      ];
+
+      $scope.leftColumn = function() {
+
+      }
    }
 ]);
 
